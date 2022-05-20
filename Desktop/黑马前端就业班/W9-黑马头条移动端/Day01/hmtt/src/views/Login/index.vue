a0b00555b4c9b89544916f18cf36516735d72f01
shaneWang <shanewang1213@gmail.com>
2022-05-20 16:17:06 +0800

    02完成登陆页的表单验证

--- Desktop/黑马前端就业班/W9-黑马头条移动端/Day01/hmtt/src/views/Login/index.vue
+++ Desktop/黑马前端就业班/W9-黑马头条移动端/Day01/hmtt/src/views/Login/index.vue
@@ -9,13 +9,13 @@
     v-model="info.mobile"
     label="手机号"
     placeholder="请输入手机号"
-    :rules="[{ required: true, message: '请填写手机号' }]"
+    :rules="rules.mobile"
   />
   <van-field
     v-model="info.code"
     label="验证码"
     placeholder="请输入验证码"
-    :rules="[{ required: true, message: '请填写验证码' }]"
+    :rules="rules.code"
   />
   <div style="margin: 16px;">
     <van-button round block type="info" native-type="submit">登录</van-button>
@@ -32,6 +32,19 @@ export default {
             info:{
                 mobile:"",
                 code:""
+            },
+            rules:{
+                // 手机号验证规则
+                mobile:[
+                    { required: true, message: '请填写手机号' },
+                    {pattern:/^1[2-9]\d{9}$/ ,message:'请输入正确的手机号'}
+                    
+                    ],
+                // 验证码验证规则
+                code:[
+                    { required: true, message: '请填写验证码' },
+                    {pattern: /^\d{6}$/,message:'请输入正确验证码'}
+                    ]
             }
         }
     },
