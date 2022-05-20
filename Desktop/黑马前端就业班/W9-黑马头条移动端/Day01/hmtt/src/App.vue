4a59598f92d837e7d92de613e74eaefc3ce09fba
shaneWang <shanewang1213@gmail.com>
2022-05-20 12:16:18 +0800

    01-项目准备

---
+++ Desktop/黑马前端就业班/W9-黑马头条移动端/Day01/hmtt/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app{
+  width: 200px;
+  height: 200px;
+  background-color: orange;
+}
+
+
+</style>
